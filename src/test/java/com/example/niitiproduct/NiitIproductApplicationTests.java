c075bf3414556d9ee5b3d2e0aba61823d6f52398
DAO TIEN TU <daotientu@gmail.com>
2023-11-12 20:11:32 +0700

    - [Init]: Commit all codes

---
+++ src/test/java/com/example/niitiproduct/NiitIproductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.niitiproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiitIproductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
