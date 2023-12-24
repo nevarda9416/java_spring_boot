128c404c467629c863573a1fe797778e4a43f53c
DAO TIEN TU <daotientu@gmail.com>
2023-12-25 00:45:27 +0700

    - [Java Framework NIIT-ICT 09]: Thymeleaf include html file

--- src/main/java/com/example/niitiproduct/controllers/api/UserController.java
+++ src/main/java/com/example/niitiproduct/controllers/api/UserController.java
@@ -1,4 +1,4 @@
-package com.example.niitiproduct.controllers.admin;
+package com.example.niitiproduct.controllers.api;
 
 import com.example.niitiproduct.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
