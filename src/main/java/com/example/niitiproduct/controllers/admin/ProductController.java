5ccc800126928fb746dd74b4489ee04a86ce56f5
DAO TIEN TU <daotientu@gmail.com>
2024-03-01 07:34:32 +0700

    - [Java Framework NIIT-ICT 09]: Store product form (Draft)

--- src/main/java/com/example/niitiproduct/controllers/admin/ProductController.java
+++ src/main/java/com/example/niitiproduct/controllers/admin/ProductController.java
@@ -1,12 +1,16 @@
 package com.example.niitiproduct.controllers.admin;
 
+import com.example.niitiproduct.dto.CategoryDTO;
 import com.example.niitiproduct.dto.ProductDTO;
 import com.example.niitiproduct.dto.PromotionDTO;
+import com.example.niitiproduct.dto.SubCategoryDTO;
 import com.example.niitiproduct.models.Product;
 import com.example.niitiproduct.models.ProductPromotion;
+import com.example.niitiproduct.services.CategoryService;
 import com.example.niitiproduct.services.ProductService;
 import com.example.niitiproduct.services.PromotionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +19,8 @@ import java.util.List;
 @Controller
 @RequestMapping("admin/products")
 public class ProductController {
+    @Autowired
+    private CategoryService categoryService;
     @Autowired
     private ProductService productService;
     @Autowired
@@ -87,7 +93,11 @@ public class ProductController {
      */
     @GetMapping("/add")
     public String add(Model model) {
+        Pageable pageable = Pageable.ofSize(100).withPage(0);
+        List<CategoryDTO> categories = categoryService.getAll(pageable);
         List<ProductDTO> products = productService.getAll();
+        model.addAttribute("categories", categories);
+        model.addAttribute("product", new ProductDTO());
         model.addAttribute("products", products);
         return "admin/product/add";
     }
